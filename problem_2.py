54561c1fab97b3b41c7d28c2cca06d7aadb53120
Harshit Samani <87422671+HarshitSamani@users.noreply.github.com>
2021-09-28 20:45:20 +0530

    Add files via upload

---
+++ problem_2.py
@@ -0,0 +1,9 @@
+from problem_2.problem_2_SVM import problem_2_SVM
+from problem_2.problem_2_DNN import problem_2_DNN
+
+
+# Problem 2 SVM
+problem_2_SVM()
+
+# # Problem 2 DNN
+# problem_2_DNN()
---
+++ problem_5.py
@@ -0,0 +1,13 @@
+from problem_5.problem_5_SVM import problem_5_SVM
+from problem_5.problem_5_DNN import problem_5_DNN
+from problem_5.problem_5_LSTM import problem_5_LSTM
+
+#
+# # Problem 5 SVM
+# problem_5_SVM()
+
+# Problem 5 DNN
+problem_5_DNN()
+
+# # Problem 5 LSTM
+# problem_5_LSTM()
